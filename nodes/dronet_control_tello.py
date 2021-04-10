8509264f483fecfd78c4e0fbdb72615c2f73eb0c
jim dim <djscut811@gmail.com>
2021-04-10 22:05:14 +0800

    debug

--- nodes/dronet_control_tello.py
+++ nodes/dronet_control_tello.py
@@ -7,7 +7,7 @@ from std_msgs.msg import Bool
 
 class Dronet_control_tello(tello.Tello):
     def __init__(self):
-        self.rate = rospy.Rate(10)
+        self.rate = rospy.Rate(1)
         rospy.Subscriber('cnn_predictions', CNN_out, self.cb_cnn_out)
         rospy.Subscriber('state_change', Bool, self.cb_state_change)
         self.desired_velocity_pub = rospy.Publisher('cnn_tello/desired_velocity', Twist, queue_size=1)
@@ -58,6 +58,9 @@ class Dronet_control_tello(tello.Tello):
             cmd_velocity_ = Twist()
             cmd_velocity_.linear.x = self.desired_forward_velocity_
             cmd_velocity_.linear.z = self.desired_angular_velocity_
+
+
+            # rospy.loginfo("cnn_out,steering_angle:%f, cal_yaw_rate:%f", self.steering_angle_, self.desired_angular_velocity_)
 
             if self.use_network_out_:
                 self.desired_velocity_pub(cmd_velocity_)
